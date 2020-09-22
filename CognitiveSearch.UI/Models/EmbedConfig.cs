4d046c3339b4ab88ecd828c73e33b3c238c1387b
Ashley KB Ow <ashley.kb.ow@avanade.com>
2020-09-22 17:17:59 +0800

    Added temp code for authenticating Power BI embedded report with a user account

---
+++ CognitiveSearch.UI/Models/EmbedConfig.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.PowerBI.Api.Models;
+using System;
+
+namespace CognitiveSearch.UI.Models
+{
+    public class EmbedConfig
+    {
+        public string Id { get; set; }
+        public string EmbedUrl { get; set; }
+
+        public EmbedToken EmbedToken { get; set; }
+
+        public int MinutesToExpiration
+        {
+            get
+            {
+                var minutesToExpiration = EmbedToken.Expiration - DateTime.UtcNow;
+                return (int)minutesToExpiration.TotalMinutes;
+            }
+        }
+        public bool? IsEffectiveIdentityRolesRequired { get; set; }
+
+        public bool? IsEffectiveIdentityRequired { get; set; }
+        
+        public bool EnableRLS { get; set; }
+
+        public string Username { get; set; }
+
+        public string Roles { get; set; }
+
+        public string ErrorMessage { get; set; }
+    }
+}
---
+++ CognitiveSearch.UI/Models/TileEmbedConfig.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.PowerBI.Api.Models;
+using System;
+
+namespace CognitiveSearch.UI.Models
+{
+    public class TileEmbedConfig : EmbedConfig
+    {
+        public string dashboardId { get; set; }
+    }
+}
