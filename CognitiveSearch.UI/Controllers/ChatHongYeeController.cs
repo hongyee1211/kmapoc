9f1f95d461b51b42bca65451ccc6ba1d23f99557
hongyee.gow@avanade.com <hongyee.gow@avanade.com>
2020-10-08 10:32:27 +0800

    commit

---
+++ CognitiveSearch.UI/Controllers/ChatHongYeeController.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+using Microsoft.AspNetCore.Mvc;
+
+namespace CognitiveSearch.UI.Controllers
+{
+    public class ChatHongYeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
