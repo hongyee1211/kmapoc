3542e2fd26b37427a0433a427abd04d563751c47
Jedrick Chua <jedrick.l.chua@accenture.com>
2020-10-09 18:53:11 +0800

    Initial commit for chat

---
+++ CognitiveSearch.UI/Controllers/ChatController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using CognitiveSearch.UI.Bot;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Graph;
+using Newtonsoft.Json;
+
+namespace CognitiveSearch.UI.Controllers
+{
+    public class ChatController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> Send([FromBody] BotRequestModel c)
+        {
+            BotRequestModel x = new BotRequestModel();
+            x.context = c.context;
+            x.text = c.text;
+            ChatModel request = new ChatModel(x);
+            //request.context["branch"] = "Query";
+            BotOutputModel output = await BotManager.Process(request);
+            return Json(output);
+        }
+    }
+
+    public class ChatbotRequestModel
+    {
+        [Required]
+        public string text { get; set; }
+
+        [Required]
+        public Hashtable context { get; set; }
+    }
+}
---
+++ CognitiveSearch.UI/wwwroot/js/chat.js
@@ -0,0 +1,25 @@
+﻿var chatContext = null;
+function sendChat() {
+    let msg = $("#chatMessage").val();
+    $.ajax({
+        type: "POST",
+        url: "chat/send",
+        data: JSON.stringify({
+            text: msg,
+            context: chatContext
+        }),
+        contentType: "application/json",
+        complete: function (data) {
+            console.log(data.responseJSON.text);
+            $('#chat-reply').text(data.responseJSON.text);
+            chatContext = data.responseJSON.context;
+            let equipments = data.responseJSON.info.EquipmentClass;
+            var listHtml = ''
+            for (let i = 0; i < equipments.length; i++) {
+                let equipment = equipments[i];
+                listHtml += `<span style="margin:3px" class="label label-info">${equipment}</span>`
+            }
+            $("#chat-equipment-class-filter-list").html(listHtml);
+        }
+    });
+}
